e4044720e1a61f64ff054068c92b873a81c3a762
Helen the Pineapple <helenahe@gmail.com>
2017-06-21 12:31:39 -0500

    Open Map JS

--- js/dummy.js
+++ js/dummy.js
@@ -1,1 +1,8 @@
 
+function openDistrictMap(){
+    document.getElementById("districtMapDiv").style.display = "inline";
+}
+
+function closeDistrictMap(){
+  document.getElementById("districtMapDiv").style.display = "none";
+}
